35eb43357950a572dfd32aa128abee0b7c16fd45
Bohdan Voronov <bohvoron@WKMZT702375F.global.publicisgroupe.net>
2024-04-06 16:20:50 +0200

    added upd 1st test and 2 jsons

--- first.spec.ts
+++ first.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('overviewTest', async ({page}) => {
-    
+
     await page.goto("https://docs.github.com/en/actions");
 
     await expect(page.locator("xpath=//*[@id='/actions/learn-github-actions/understanding-github-actions']")).toBeVisible();
@@ -10,5 +10,6 @@ test('overviewTest', async ({page}) => {
     
     await buttonOverview.click();
     await expect(page.locator("xpath=//*[@id='title-h1']")).toHaveText("Understanding GitHub Actions");
+    
 });
 
